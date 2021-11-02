df96f91e91c6767697f98afb75926a9bbbd852fa
jaikesJT <jakob.tossebro@gmail.com>
2021-11-03 00:53:58 +0100

    started working on outside button script

--- Assets/Scripts/OutsideButtonHandler.cs
+++ Assets/Scripts/OutsideButtonHandler.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-public class Button : MonoBehaviour
+public class OutsideButtonHandler : MonoBehaviour
 {
+    public GameObject outsideButton;
+    public GameObject middleButton;
+
     // Start is called before the first frame update
     void Start()
-    {
+    {       
+        outsideButton = GameObject.find("Outside Button");
+        middleButton = GameObject.find("Middle Button")
         //The button shouldn't be there until the player clicks the door. If the player clicks the button, the door unlocks and the button vanishes. 
         //If the player clicks outside the button the button vanishes again, without unlocking the door.
     }
@@ -16,4 +22,9 @@ public class Button : MonoBehaviour
     {
         
     }
+
+    
+
+    
+       
 }
